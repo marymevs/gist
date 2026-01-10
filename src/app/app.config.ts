c0ba218bd8ac1a54fb3e03bc533fbcfb5cef29cc
Mary Imevbore <mimevbore14@gmail.com>
2026-01-10 12:29:49 -0500

    fix build issue

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,13 +2,30 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
-import { provideFirestore, getFirestore, connectFirestoreEmulator } from '@angular/fire/firestore';
-import { provideFunctions, getFunctions, connectFunctionsEmulator} from '@angular/fire/functions';
+import {
+  provideFirestore,
+  getFirestore,
+  connectFirestoreEmulator,
+} from '@angular/fire/firestore';
+import {
+  provideFunctions,
+  getFunctions,
+  connectFunctionsEmulator,
+} from '@angular/fire/functions';
 import { provideMessaging, getMessaging } from '@angular/fire/messaging';
-import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+import {
+  provideStorage,
+  getStorage,
+  connectStorageEmulator,
+} from '@angular/fire/storage';
 import { routes } from './app.routes';
 import { provideRouter } from '@angular/router';
-import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import {
+  getAnalytics,
+  provideAnalytics,
+  ScreenTrackingService,
+  UserTrackingService,
+} from '@angular/fire/analytics';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 
 export const appConfig: ApplicationConfig = {
@@ -19,6 +36,6 @@ export const appConfig: ApplicationConfig = {
     provideFunctions(() => getFunctions()),
     provideMessaging(() => getMessaging()),
     provideStorage(() => getStorage()),
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"gist-ab4e8","appId":"1:46121169736:web:12b677570dc805f0efb97d","storageBucket":"gist-ab4e8.firebasestorage.app","apiKey":"AIzaSyAK29USenxgvdh9qYKiM7ibepPB9rkWLYM","authDomain":"gist-ab4e8.firebaseapp.com","messagingSenderId":"46121169736","measurementId":"G-VJ1XNNM6M5","projectNumber":"46121169736","version":"2"})), provideAuth(() => getAuth()), provideAnalytics(() => getAnalytics()), ScreenTrackingService, UserTrackingService, provideFirestore(() => getFirestore()), provideFunctions(() => getFunctions()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
+    provideRouter(routes),
   ],
 };
